beeeb59d9d24a48ec5c4cf47dae913baae6c9bfa
MohammadLatifi <irancodedeveloper@gmail.com>
2024-03-04 19:32:41 +0100

    added some more custome error pages

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -5,6 +5,7 @@ namespace App\EventSubscriber;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ExceptionSubscriber extends AbstractController implements EventSubscriberInterface
@@ -19,11 +20,20 @@ class ExceptionSubscriber extends AbstractController implements EventSubscriberI
     public function giveResponse(ExceptionEvent $event): ExceptionEvent
     {
         $throwable = $event->getThrowable();
+
         if ($throwable instanceof NotFoundHttpException) {
             $event->setResponse($this->render('exceptions/error404.html.twig'));
 
             return $event;
         }
+
+        if ($throwable instanceof AccessDeniedHttpException) {
+            $event->setResponse($this->render('exceptions/error403.html.twig'));
+
+            return $event;
+        }
+
+        $event->setResponse($this->render('exceptions/error.html.twig'));
 
         return $event;
     }
